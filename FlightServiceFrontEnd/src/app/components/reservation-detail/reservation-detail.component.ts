72dac82670d3b1b423b3b9c707574d142efef880
Colin McGregor <colin.w.mcgregor@gmail.com>
2022-08-17 11:01:23 -0500

    reservation details working

--- FlightServiceFrontEnd/src/app/components/reservation-detail/reservation-detail.component.ts
+++ FlightServiceFrontEnd/src/app/components/reservation-detail/reservation-detail.component.ts
@@ -15,33 +15,24 @@ import { PassengerService } from 'src/app/services/passenger.service';
   styleUrls: ['./reservation-detail.component.css']
 })
 export class ReservationDetailComponent implements OnInit {
-@Input()reservation?:Reservation;
-@Input()flight?:Flight;
-@Input()passenger?:Passenger;
+reservation?:Reservation;
+flight?:Flight;
+passenger?:Passenger;
+id?:number;
   constructor(private route: ActivatedRoute, private flightService: FlightService, private location:Location, private router:Router, private passengerService:PassengerService,private reservationService:ReservationService) { }
 
   ngOnInit(): void {
     this.getReservation();
-    // this.getFlight();
-    // this.getPassenger();
   }
   getReservation():void{
-    const id=Number(this.route.snapshot.paramMap.get('id'));
-    this.reservationService.getReservation(id).subscribe(reservation=>this.reservation=reservation)
-    const flightId=Number(this.reservation?.flightId);
-    this.flightService.getFlight(flightId).subscribe(flight=>this.flight=flight)
-    const passengerId=Number(this.reservation?.passengerId);
-    this.passengerService.getPassenger(passengerId).subscribe(passenger=>this.passenger=passenger)
+    this.id=Number(this.route.snapshot.paramMap.get('id'));
+    this.reservationService.getReservation(this.id).subscribe(reservation=>{
+      this.reservation=reservation;
+      let flightId=Number(this.reservation?.flightId);
+    this.flightService.getFlight(flightId).subscribe(flight=>this.flight=flight);
+    let passengerId=Number(this.reservation?.passengerId);
+    this.passengerService.getPassenger(passengerId).subscribe(passenger=>this.passenger=passenger);
+    });
   }
-
-  // getFlight():void{
-  //   const flightId=Number(this.reservation?.flightId);
-  //   this.flightService.getFlight(flightId).subscribe(flight=>this.flight=flight)
-  // }
-
-  // getPassenger():void{
-  //   const passengerId=Number(this.reservation?.passengerId);
-  //   this.passengerService.getPassenger(passengerId).subscribe(passenger=>this.passenger=passenger)
-  // }
 
 }
